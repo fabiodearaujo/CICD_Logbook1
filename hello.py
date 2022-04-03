0befc0521850156b294024be42fb1a47bb9964b2
fabiodearaujo <L00169711@student.lyit.ie>
2022-04-03 14:37:18 +0100

    putting errors to hello.py to check lint is working

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
+
+
+
 #Adding simple Phython hello world program for testing linter.
 def send_message(message):
     print(f"Your message is: {message}")
+     return  message
+
+
+
 
 send_message("Hello World!")
