652fa2691cc9610fa028c0ac224256ec54fc9a73
fabiodearaujo <L00169711@student.lyit.ie>
2022-04-03 15:55:52 +0100

    added a space after # as sugested by the linter

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-#Adding simple Phython hello world program for testing linter.
+# Adding simple Phython hello world program for testing linter.
 def send_message(message):
     print(f"Your message is: {message}")
 
