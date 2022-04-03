d9a2de44a59ce67bce21d768b268bf705c369f67
fabiodearaujo <L00169711@student.lyit.ie>
2022-04-03 15:31:45 +0100

    changed the workflow name and fixed the python code to check if linter now pass and deploy to DEV

--- hello.py
+++ hello.py
@@ -1,12 +1,7 @@
-
-
-
 #Adding simple Phython hello world program for testing linter.
 def send_message(message):
     print(f"Your message is: {message}")
-     return  message
 
 
-
-
-send_message("Hello World!")
+my_message = "More Python, more fun!"
+send_message(my_message)
